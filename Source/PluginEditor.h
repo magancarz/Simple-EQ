2bfb2b0750a67c065e26c36f0d787d1ba4cc4b24
magancarz <grzmateusz6@gmail.com>
2022-11-22 14:43:43 +0100

    fixed response curve bug when first opening the GUI

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -69,6 +69,8 @@ private:
     juce::Atomic<bool> parametersChanged { false };
 
     MonoChain monoChain;
+
+    void updateChain();
 };
 
 //==============================================================================
