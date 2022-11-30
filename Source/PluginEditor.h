3ace547bbff5b54c966f2d1a06fb0d63dc7b5e24
magancarz <grzmateusz6@gmail.com>
2022-11-30 15:46:16 +0100

    added grid lines to the background of the waveform

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -64,6 +64,8 @@ struct ResponseCurveComponent: juce::Component,
     void timerCallback() override;
 
     void paint(juce::Graphics& g) override;
+
+    void resized() override;
 private:
     SimpleEQAudioProcessor& audioProcessor;
     juce::Atomic<bool> parametersChanged { false };
@@ -71,6 +73,12 @@ private:
     MonoChain monoChain;
 
     void updateChain();
+
+    juce::Image background;
+
+    juce::Rectangle<int> getRenderArea();
+
+    juce::Rectangle<int> getAnalysisArea();
 };
 
 //==============================================================================
