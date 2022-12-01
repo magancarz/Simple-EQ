a7ebeb557f6b990a1a43f05eeb57cabe801e92b6
magancarz <grzmateusz6@gmail.com>
2022-12-01 16:54:40 +0100

    implemented analyzer button

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -267,6 +267,9 @@ private:
 };
 
 //==============================================================================
+struct PowerButton : juce::ToggleButton {};
+struct AnalyzerButton : juce::ToggleButton {};
+
 /**
 */
 class SimpleEQAudioProcessorEditor  : public juce::AudioProcessorEditor {
@@ -304,7 +307,8 @@ private:
                lowCutSlopeSliderAttachment,
                highCutSlopeSliderAttachment;
 
-    juce::ToggleButton lowcutBypassButton, peakBypassButton, highcutBypassButton, analyzerEnabledButton;
+    PowerButton lowcutBypassButton, peakBypassButton, highcutBypassButton;
+    AnalyzerButton analyzerEnabledButton;
 
     using ButtonAttachment = APVTS::ButtonAttachment;
     ButtonAttachment lowcutBypassButtonAttachment,
